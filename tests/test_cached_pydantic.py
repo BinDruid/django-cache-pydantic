483a9b3d6f2be75d42191edfb25dbf82496497e4
BinDruid <abharya.dev@gmail.com>
2024-03-14 18:21:13 +0330

    Update phone number in tests

--- tests/test_cached_pydantic.py
+++ tests/test_cached_pydantic.py
@@ -30,7 +30,7 @@ class TestCachedPydanticModel(SimpleTestCase):
         default_cache.clear()
 
     def test_create_new_cache_instance_via_object_manager(self):
-        creation_context = {'username': 'ali', 'mobile_number': '09124318673'}
+        creation_context = {'username': 'ali', 'mobile_number': '09304444444'}
         new_instance = SampleTestModel.objects.create(**creation_context)
         cache_instance = default_cache.get(new_instance.pk)
         value_under_test = new_instance.pk
@@ -38,7 +38,7 @@ class TestCachedPydanticModel(SimpleTestCase):
         self.assertEqual(value_under_test, value_expected, msg='Did not get expected from cache')
 
     def test_create_new_cache_instance_via_save_method(self):
-        creation_context = {'username': 'ali', 'mobile_number': '09124318673'}
+        creation_context = {'username': 'ali', 'mobile_number': '09304444444'}
         new_instance = SampleTestModel(**creation_context)
         new_instance.save()
         cache_instance = default_cache.get(new_instance.pk)
@@ -47,7 +47,7 @@ class TestCachedPydanticModel(SimpleTestCase):
         self.assertEqual(value_under_test, value_expected, msg='Did not get expected from cache')
 
     def test_retrieve_cache_instance_via_get_method(self):
-        creation_context = {'username': 'ali', 'mobile_number': '09124318673'}
+        creation_context = {'username': 'ali', 'mobile_number': '09304444444'}
         new_instance = SampleTestModel.objects.create(**creation_context)
         cache_instance = SampleTestModel.objects.get(new_instance.pk)
         value_under_test = new_instance.pk
@@ -55,7 +55,7 @@ class TestCachedPydanticModel(SimpleTestCase):
         self.assertEqual(value_under_test, value_expected, msg='Did not get expected from cache')
 
     def test_cache_instance_still_in_cache_before_ttl(self):
-        creation_context = {'username': 'ali', 'mobile_number': '09124318673'}
+        creation_context = {'username': 'ali', 'mobile_number': '09304444444'}
         new_instance = SampleTestModel.objects.create(**creation_context)
         with time_machine.travel(new_instance.created_at, tick=False) as time_traveller:
             time_shift = datetime.timedelta(seconds=default_ttl - 20)
